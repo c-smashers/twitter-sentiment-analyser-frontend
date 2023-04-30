8cc8c021c67f029d3afda2df7ebeb77b6a60e13c
hemant1101 <hemantmandal1101@gmail.com>
2023-05-01 00:20:02 +0530

    hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
 import Home from './pages/home/Home'
@@ -8,7 +8,7 @@ import TextSentimentAnalyser from './pages/textSentiment/TextSentimentAnalyser'
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -18,7 +18,7 @@ const App = () => {
         <Route path="*" element={<Home />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
